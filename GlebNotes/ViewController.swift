1e4f690e02fe25a5fdac1c11ac0d15497577a06a
GlebRed <glebredko@gmail.com>
2018-12-09 16:54:21 +0200

    Initial Commit

---
+++ GlebNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GlebNotes
+//
+//  Created by Gleb Redko on 09/12/2018.
+//  Copyright © 2018 Gleb Redko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
